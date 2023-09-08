51ae0d99b4ff59215e1aa040d6ec65754cb0a80e
En3rGy <en3rgy@web.de>
2023-09-08 23:52:34 +0200

    added multi language support for HS-module

--- 01 build.cmd
+++ 01 build.cmd
@@ -2,18 +2,19 @@
 set path=%path%;C:\Python27\
 set PYTHONPATH=C:\Python27;C:\Python27\Lib
 
+rm .\release\log14401.html
 echo ^<head^> > .\release\log14401.html
-echo ^<link rel="stylesheet" href="style.css"^> >> .\release\log14401.html
-echo ^<title^>Logik - Feiertage / Ferien (14401)^</title^> >> .\release\log14401.html
-echo ^<style^> >> .\release\log14401.html
-echo body { background: none; } >> .\release\log14401.html
-echo ^</style^> >> .\release\log14401.html
-echo ^<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"^> >> .\release\log14401.html
-echo ^</head^> >> .\release\log14401.html
+echo ^<link rel="stylesheet" href="style.css"^> >> .\release\DE-log14401.html
+echo ^<title^>Logik - Feiertage / Ferien (14401)^</title^> >> .\release\DE-log14401.html
+echo ^<style^> >> .\release\DE-log14401.html
+echo body { background: none; } >> .\release\DE-log14401.html
+echo ^</style^> >> .\release\DE-log14401.html
+echo ^<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"^> >> .\release\DE-log14401.html
+echo ^</head^> >> .\release\DE-log14401.html
 
 @echo on
 
-type .\README.md | C:\Python27\python -m markdown -x tables >> .\release\log14401.html
+type .\README.md | C:\Python27\python -m markdown -x tables >> .\release\DE-log14401.html
 
 cd ..\..
 C:\Python27\python generator.pyc "14401_FeiertageFerien" UTF-8
